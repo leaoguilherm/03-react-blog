fa9685715aaeaaf2b9cf603c5b6500cabea10f6d
leaoguilherm <guilhermeleao98@outlook.com>
2023-05-15 19:14:23 -0300

    Atualizando Create Post

--- src/pages/CreatePost/index.js
+++ src/pages/CreatePost/index.js
@@ -11,6 +11,8 @@ export const CreatePost = () => {
     const [valueDescription, setValueDescription] = useState('')
     const [valueAuthor, setValueAuthor] = useState('')
 
+    const [elementStyle, setElementStyle] = useState ('none')
+
     const changeTitle = (event) => {
         setValueTitle(event.target.value)
     }
@@ -29,43 +31,61 @@ export const CreatePost = () => {
             description: valueDescription,
             author: valueAuthor
         };
-
-        await postApi(newPost)
+        if (valueTitle !== '' && valueDescription !== '' && valueAuthor !== '') { 
+            await postApi(newPost)
+            setElementStyle('block')
+            setValueTitle('')
+            setValueDescription('')
+            setValueAuthor('')
+        }
+        
     }
 
+    const formInput = (event) => {
+        event.preventDefault()
+    }
 
 
     return (
         <div className="containerCreatePost">
             <h3 className="titleCreatePost">Cadastro de posts</h3>
-            <div className="containerInput">
-                <Input
-                    htmlFor="title"
-                    label="Título"
-                    className="inputTitle"
-                    name="title"
-                    placeholder="Escreva o título do post"
-                    onChange={changeTitle} />
+            <form onSubmit={formInput}>
+                <div className="containerInput">
+                    <Input
+                        htmlFor="title"
+                        label="Título"
+                        className="inputTitle"
+                        name="title"
+                        placeholder="Escreva o título do post"
+                        onChange={changeTitle}
+                        value={valueTitle}
+                        required />
 
-                <Input
-                    htmlFor="description"
-                    label="Descrição"
-                    className="inputDescription"
-                    name="description"
-                    placeholder="Escreva a descrição do post"
-                    onChange={changeDescription} />
+                    <Input
+                        htmlFor="description"
+                        label="Descrição"
+                        className="inputDescription"
+                        name="description"
+                        placeholder="Escreva a descrição do post"
+                        onChange={changeDescription}
+                        value={valueDescription}
+                        required />
 
-                <Input
-                    htmlFor="author"
-                    label="Autor"
-                    className="inputAuthor"
-                    name="author"
-                    placeholder="Escreva o autor do post"
-                    onChange={changeAuthor} />
-            </div>
-            <div className="container-buttonCreatePost">
-                <Button className="buttonCreatePost" onClick={clickCreatePost}>Cadastrar post</Button>
-            </div>
+                    <Input
+                        htmlFor="author"
+                        label="Autor"
+                        className="inputAuthor"
+                        name="author"
+                        placeholder="Escreva o autor do post"
+                        onChange={changeAuthor}
+                        value={valueAuthor}
+                        required />
+                </div>
+                <div className="successMessage" style={{ display: elementStyle }}>Publicação realizada com sucesso.</div>
+                <div className="container-buttonCreatePost">
+                    <Button className="buttonCreatePost" onClick={clickCreatePost} typ="submit">Cadastrar post</Button>
+                </div>
+            </form>
         </div>
     )
 }
